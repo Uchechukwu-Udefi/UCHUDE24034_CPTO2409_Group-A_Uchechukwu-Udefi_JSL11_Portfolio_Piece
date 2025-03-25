f4b7c1c39dd7c8ae5796be5a010f19d00eb24d37
Uchechukwu-Udefi <udefihenry@gmail.com>
2025-03-25 19:26:15 +0200

    Import functions and data file

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // TASK: import helper functions from utils
 // TASK: import initialData
+import { getTasks, createNewTask, patchTask, putTask, deleteTask } from './utils.js';
+import { initialData } from './initialData.js';
 
 
 /*************************************************************************************************************************************************
