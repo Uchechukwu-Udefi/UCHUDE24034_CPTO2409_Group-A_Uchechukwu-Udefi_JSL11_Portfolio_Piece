16da55215ccd767d513032d05828ec84d72327d9
Uchechukwu-Udefi <udefihenry@gmail.com>
2025-03-27 09:06:03 +0200

    Update funtion to add task to UI

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ function styleActiveBoard(boardName) {
 
 
 function addTaskToUI(task) {
-  const column = document.querySelector('.column-div[data-status="${task.status}"]'); 
+  const column = document.querySelector(`.column-div[data-status="${task.status}"]`); 
   if (!column) {
     console.error(`Column not found for status: ${task.status}`);
     return;
@@ -150,7 +150,11 @@ function addTaskToUI(task) {
   taskElement.textContent = task.title; // Modify as needed
   taskElement.setAttribute('data-task-id', task.id);
   
-  tasksContainer.appendChild(); 
+  taskElement.addEventListener("click", () => {
+    openEditTaskModal(task);
+  });
+  
+  tasksContainer.appendChild(taskElement); 
 }
 
 
